ca2be8b730458292205ce142aaacc66c1bd08c0d
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2025-05-22 00:36:34 +0300

    Fix Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, create_engine, Session
 
-# 💥 ВЖАРЮЄМО абсолютний шлях до каталогу, де main.py
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
